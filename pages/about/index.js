5a78962e14d619a2d96e70cef03df3a5fce4e27c
julian <julian@julians-MacBook-Pro.local>
2022-01-21 14:06:52 -0700

    <Build: initial setup for the page, downloading sass

---
+++ pages/about/index.js
@@ -0,0 +1,8 @@
+import styles from "./about.module.scss";
+import { Fragment } from "react";
+
+const About = () => {
+  return <Fragment></Fragment>;
+};
+
+export default About;
---
+++ pages/index.js
@@ -0,0 +1,7 @@
+// import "./global.css";
+// import
+// const HomePage = () => {
+//   return <h1>Home page</h1>;
+// };
+
+// export default HomePage;
