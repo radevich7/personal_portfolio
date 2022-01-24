6aa6dcc2842d6838c1359338f7eceef8e5e706df
Julian Radevych <radevich7@gmail.com>
2022-01-24 14:14:36 -0700

    <Build: Navigation links container>

--- components/layout/Navigation.js
+++ components/layout/Navigation.js
@@ -6,36 +6,40 @@ const Navigation = () => {
     <div className={styles.nav}>
       <div className={styles.nav__logo}>
         <Link href="/">
-          <span className={styles.nav__logo__element1}>J</span>
+          <a className={styles.nav__logo__element1}>J</a>
         </Link>
         <span className={styles.nav__logo__element2}>Julian</span>
         <span className={styles.nav__logo__element3}>Web Developer</span>
       </div>
-      <nav className={styles.navigation}>
-        <ul className="navigation__list">
-          <li className="navigation__item">
-            <Link href="#1" className="navigation__link">
-              About
-            </Link>
-          </li>
-          <li className="navigation__item">
-            <Link href="#2" className="navigation__link">
-              My skills
-            </Link>
-          </li>
 
-          <li className="navigation__item">
-            <Link href="#4" className="navigation__link">
-              Contact
-            </Link>
-          </li>
-          <li className="navigation__item">
-            <Link href="#5" className="navigation__link">
-              Follow up links
-            </Link>
-          </li>
-        </ul>
-      </nav>
+      <ul className={styles.nav__list}>
+        <li className={styles.nav__list__item}>
+          <Link href="/about">
+            <a className={styles.nav__list__link}> About me</a>
+          </Link>
+        </li>
+        <li className={styles.nav__list__item}>
+          <Link href="/skills">
+            <a className={styles.nav__list__link}> Skills</a>
+          </Link>
+        </li>
+        <li className={styles.nav__list__item}>
+          <Link href="/portfolio">
+            <a className={styles.nav__list__link}>Portfolio</a>
+          </Link>
+        </li>
+        <li className={styles.nav__list__item}>
+          <Link href="/resume">
+            <a className={styles.nav__list__link}>CV</a>
+          </Link>
+        </li>
+
+        <li className={styles.nav__list__item}>
+          <Link href="/contact">
+            <a className={styles.nav__list__link}>Contact</a>
+          </Link>
+        </li>
+      </ul>
     </div>
   );
 };
