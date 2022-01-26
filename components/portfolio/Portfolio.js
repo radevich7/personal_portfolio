9045b99bff1c81765e5d498b6c1d2130c0b14315
Julian Radevych <radevich7@gmail.com>
2022-01-26 12:30:10 -0700

    <Build: portfolio component set up>

---
+++ components/portfolio/Portfolio.js
@@ -0,0 +1,8 @@
+import styles from "./Portfolio.module.scss";
+import Link from "next/link";
+
+const Portfolio = () => {
+  return <section className={styles.portfolio}></section>;
+};
+
+export default Portfolio;
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import { Fragment } from "react";
 import About from "../components/about/About";
 import Header from "../components/header/Header";
+import Portfolio from "../components/portfolio/Portfolio";
 const HomePage = () => {
   return (
     <Fragment>
       <Header />
       <About />
+      <Portfolio />
     </Fragment>
   );
 };
