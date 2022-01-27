8754ac32d249814238828b22cb490aa7b6dae7bd
Julian Radevych <radevich7@gmail.com>
2022-01-27 16:06:45 -0700

    <Build: grid layout set up>

--- components/portfolio/Portfolio.js
+++ components/portfolio/Portfolio.js
@@ -2,7 +2,62 @@ import styles from "./Portfolio.module.scss";
 import Link from "next/link";
 
 const Portfolio = () => {
-  return <section className={styles.portfolio}></section>;
+  return (
+    <section className={styles.portfolio}>
+      <h2>Portfolio</h2>
+      <p>
+        Below you can see some of my projects, that I've been working lately
+      </p>
+
+      <ul className={styles.portfolio__container}>
+        <li className={styles.portfolio__item__1}>
+          <img
+            className={styles.portfolio__image}
+            src="/MKTFY.png"
+            alt="Picture of the project 1"
+          />
+        </li>
+        <li className={styles.portfolio__item__2}>
+          <img
+            className={styles.portfolio__image}
+            src="/MKTFY.png"
+            alt="Picture of the project 1"
+          />
+        </li>
+        <li className={styles.portfolio__item__3}>
+          <img
+            className={styles.portfolio__image}
+            src="/MKTFY.png"
+            alt="Picture of the project 1"
+          />
+        </li>
+        <li className={styles.portfolio__item__4}>
+          {" "}
+          <img
+            className={styles.portfolio__image}
+            src="/MKTFY.png"
+            alt="Picture of the project 1"
+          />
+        </li>
+        <li className={styles.portfolio__item__5}>
+          {" "}
+          <img
+            className={styles.portfolio__image}
+            src="/MKTFY.png"
+            alt="Picture of the project 1"
+          />
+        </li>
+        <li className={styles.portfolio__item__6}>
+          {" "}
+          <img
+            className={styles.portfolio__image}
+            src="/MKTFY.png"
+            alt="Picture of the project 1"
+          />
+        </li>
+      </ul>
+    </section>
+  );
 };
 
 export default Portfolio;
