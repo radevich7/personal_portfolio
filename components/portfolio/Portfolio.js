4b1d6921f80a1eb8e58adfeefdefbaaf6c9a5671
Julian Radevych <radevich7@gmail.com>
2022-01-28 14:50:14 -0700

    <Build:portfolio page ui , created  video elements and styled them>

--- components/portfolio/Portfolio.js
+++ components/portfolio/Portfolio.js
@@ -1,5 +1,6 @@
 import styles from "./Portfolio.module.scss";
 import Link from "next/link";
+import { FaArrowCircleRight } from "react-icons/fa";
 
 const Portfolio = () => {
   return (
@@ -12,86 +13,112 @@ const Portfolio = () => {
       <ul className={styles.portfolio__container}>
         <li className={styles.portfolio__item__1}>
           <div className={styles.portfolio__hoverContent}>
-            <img
-              className={styles.portfolio__image}
-              src="/projectNexter1.png"
-              alt="Picture of the project 1"
-            />
-            <Link href="/">
-              <a className={styles.portfolio__image__link}>
-                <span> View project</span>
-              </a>
-            </Link>
+            <div className={styles.portfolio__video}>
+              <video
+                className={styles.portfolio__video__content}
+                loop
+                autoPlay
+                muted
+              >
+                <source src="reactMeals.webm" type="video/webm" />
+                <source src="reactMeals.mp4" type="video/mp4" />
+              </video>
+              <Link href="/">
+                <a className={styles.portfolio__video__link}>
+                  <FaArrowCircleRight />
+                </a>
+              </Link>
+            </div>
           </div>
         </li>
         <li className={styles.portfolio__item__2}>
           <div className={styles.portfolio__hoverContent}>
-            <img
-              className={styles.portfolio__image}
-              src="/projectMapty.png"
-              alt="Picture of the project 2"
-            />
-            <Link href="/">
-              <a className={styles.portfolio__image__link}>
-                <span> View project</span>
-              </a>
-            </Link>
+            <div className={styles.portfolio__video}>
+              <video
+                className={styles.portfolio__video__content}
+                loop
+                autoPlay
+                muted
+              >
+                <source src="mapty.webm" type="video/webm" />
+                <source src="mapty.mp4" type="video/mp4" />
+                Your browser is not supported!
+              </video>
+              <Link href="/">
+                <a className={styles.portfolio__video__link}>
+                  <FaArrowCircleRight />
+                </a>
+              </Link>
+            </div>
           </div>
         </li>
         <li className={styles.portfolio__item__3}>
           <div className={styles.portfolio__hoverContent}>
-            <img
-              className={styles.portfolio__image}
-              src="/MKTFY.png"
-              alt="Picture of the project 3"
-            />
-            <Link href="/">
-              <a className={styles.portfolio__image__link}>
-                <span> View project</span>
-              </a>
-            </Link>
+            <div className={styles.portfolio__video}>
+              <video
+                className={styles.portfolio__video__content}
+                loop
+                autoPlay
+                muted
+              >
+                <source src="mktfy.webm" type="video/webm" />
+                <source src="mktfy.mp4" type="video/mp4" />
+                Your browser is not supported!
+              </video>
+              <Link href="/">
+                <a className={styles.portfolio__video__link}>
+                  <FaArrowCircleRight />
+                </a>
+              </Link>
+            </div>
           </div>
         </li>
         <li className={styles.portfolio__item__4}>
           <div className={styles.portfolio__hoverContent}>
-            <img
-              className={styles.portfolio__image}
-              src="/projectFood.png"
-              alt="Picture of the project 4"
-            />
-            <Link href="/">
-              <a className={styles.portfolio__image__link}>
-                <span> View project</span>
-              </a>
-            </Link>
+            <div className={styles.portfolio__video}>
+              <video
+                className={styles.portfolio__video__content}
+                loop
+                autoPlay
+                muted
+              >
+                <source src="nexter.mp4" type="video/mp4" />
+                <source src="nexter.webm" type="video/webm" />
+                Your browser is not supported!
+              </video>
+            </div>
           </div>
         </li>
         <li className={styles.portfolio__item__5}>
           <div className={styles.portfolio__hoverContent}>
-            <img
-              className={styles.portfolio__image}
-              src="/projectNatour.png"
-              alt="Picture of the project 5"
-            />
-            <Link href="/">
-              <a className={styles.portfolio__image__link}>
-                <span> View project</span>
-              </a>
-            </Link>
+            <div className={styles.portfolio__video}>
+              <video
+                className={styles.portfolio__video__content}
+                loop
+                autoPlay
+                muted
+              >
+                <source src="natours.mp4" type="video/mp4" />
+                <source src="natours.webm" type="video/webm" />
+                Your browser is not supported!
+              </video>
+            </div>
           </div>
         </li>
         <li className={styles.portfolio__item__6}>
           <div className={styles.portfolio__hoverContent}>
-            <img
-              className={styles.portfolio__image}
-              src="/projectNext.png"
-              alt="Picture of the project 6"
-            />
-            <Link href="/">
-              <a className={styles.portfolio__image__link}>
-                <span> View project</span>
-              </a>
-            </Link>
+            <div className={styles.portfolio__video}>
+              <video
+                className={styles.portfolio__video__content}
+                loop
+                autoPlay
+                muted
+              >
+                <source src="meetups.mp4" type="video/mp4" />
+                <source src="meetups.webm" type="video/webm" />
+                Your browser is not supported!
+              </video>
+            </div>
           </div>
         </li>
       </ul>
