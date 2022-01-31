93b41f772a4806ec19d785ca02c81d600f263538
Julian Radevych <radevich7@gmail.com>
2022-01-31 12:29:29 -0700

    <Build: set up skills layout, added all the icons>

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,14 @@ import { Fragment } from "react";
 import About from "../components/about/About";
 import Header from "../components/header/Header";
 import Portfolio from "../components/portfolio/Portfolio";
+import Skills from "../components/skills/Skills";
 const HomePage = () => {
   return (
     <Fragment>
       <Header />
+
       <About />
+      <Skills />
       <Portfolio />
     </Fragment>
   );
