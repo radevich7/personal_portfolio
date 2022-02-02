e87dcece4c92d6261669733986979cc829241dd6
Julian Radevych <radevich7@gmail.com>
2022-02-02 11:57:21 -0700

    <Build:added popup for the contact form>

--- components/contactForm/ContactForm.js
+++ components/contactForm/ContactForm.js
@@ -1,13 +1,17 @@
+import { useState } from "react";
 import styles from "./ContactForm.module.scss";
 import * as Yup from "yup";
 import { Formik, Form, useField } from "formik";
 import Swal from "sweetalert2";
 import emailjs from "emailjs-com";
-
+import "animate.css";
+import { BallTriangle } from "react-loader-spinner";
+// Emailjs service credentials
 const SERVICE_ID = "service_5un9za6";
 const TEMPLATE_ID = "template_oj2w0pc";
 const USER_ID = "user_nL84NBjg7D93QkB4gd3oe";
-// Input
+
+//  TEXT input
 const MyTextInput = ({ label, ...props }) => {
   const [field, meta] = useField(props);
 
@@ -23,6 +27,7 @@ const MyTextInput = ({ label, ...props }) => {
     </>
   );
 };
+// TEXTAREA input
 const MyTextArea = ({ label, ...props }) => {
   const [field, meta] = useField(props);
   return (
@@ -38,84 +43,130 @@ const MyTextArea = ({ label, ...props }) => {
   );
 };
 
+// CONTACT FORM
+
 const ContactForm = () => {
-  const sendEmail = (data) => {
-    emailjs.send(SERVICE_ID, TEMPLATE_ID, data, USER_ID).then(
-      function (response) {
-        console.log("SUCCESS!", response.status, response.text);
-      },
-      function (error) {
-        console.log("FAILED...", error);
-      }
-    );
+  const [loading, setLoading] = useState(false);
+  const sendEmail = async (data) => {
+    setLoading(true);
+    const sendData = () =>
+      emailjs.send(SERVICE_ID, TEMPLATE_ID, data, USER_ID).then(
+        function (response) {
+          setLoading(false);
+          Swal.fire({
+            icon: "success",
+            title: "Message Sent Successfully",
+            showClass: {
+              popup: "animate__animated animate__fadeInDownBig",
+            },
+            hideClass: {
+              popup: "animate__animated animate__fadeOutUpBig",
+            },
+            color: "#ffffff",
+            background: "#072227",
+            confirmButtonColor: "#208294",
+          });
+        },
+        function (error) {
+          setLoading(false);
+          Swal.fire({
+            icon: "error",
+            title:
+              "Ooops, something went wrong, please contact me directly at radevich7@gmail.com",
+
+            showClass: {
+              popup: "animate__animated animate__fadeInDownBig",
+            },
+            hideClass: {
+              popup: "animate__animated animate__fadeOutUpBig",
+            },
+            color: "#ffffff",
+            background: "#072227",
+            confirmButtonColor: "#208294",
+          });
+        }
+      );
+    sendData();
+    return sendData;
   };
+
   return (
     <section className={styles.form}>
       <h1 className={styles.form__heading1}>Send me a message!</h1>
       <h2 className={styles.form__heading2}>
         Got a question or proposal, or just want to say hello? Go ahead!
       </h2>
-      <Formik
-        initialValues={{
-          name: "",
-          email: "",
-          message: "",
-        }}
-        validationSchema={Yup.object({
-          name: Yup.string(25, "Must be 15 characters or less").required(
-            "Required"
-          ),
-          email: Yup.string()
-            .email("Invalid email address")
-            .required("Required"),
-          message: Yup.string(250, "Must be 250 characters or less").required(
-            "Required"
-          ),
-        })}
-        onSubmit={async (values, { setSubmitting, resetForm }) => {
-          await new Promise((r) => setTimeout(r, 500));
-          setSubmitting(false);
-          // sendEmail(values);
-          console.log(values);
-          resetForm({ values: "" });
-        }}
-      >
-        <Form className={styles.form__container}>
-          <div className={styles.form__container__name}>
-            <MyTextInput
-              label="Full Name"
-              id="name"
-              name="name"
-              type="text"
-              placeholder="Full Name"
-              className={styles.form__input__name}
-            />
-          </div>
-          <div className={styles.form__container__email}>
-            <MyTextInput
-              label="Email Address"
-              id="email"
-              name="email"
-              type="email"
-              placeholder="Email Address"
-            />
-          </div>
-          <div className={styles.form__container__message}>
-            <MyTextArea
-              label="Message"
-              name="message"
-              id="message"
-              placeholder="Message"
-              rows="4"
-              cols="50"
-            />
-          </div>
+      {loading ? (
+        <div className={styles.spinner}>
+          <BallTriangle
+            heigth="150"
+            width="150"
+            color="#208294"
+            ariaLabel="loading"
+          />
+        </div>
+      ) : (
+        <Formik
+          initialValues={{
+            name: "",
+            email: "",
+            message: "",
+          }}
+          validationSchema={Yup.object({
+            name: Yup.string(25, "Must be 15 characters or less").required(
+              "Required"
+            ),
+            email: Yup.string()
+              .email("Invalid email address")
+              .required("Required"),
+            message: Yup.string(250, "Must be 250 characters or less").required(
+              "Required"
+            ),
+          })}
+          onSubmit={async (values, { setSubmitting, resetForm }) => {
+            await new Promise((r) => setTimeout(r, 500));
+            setSubmitting(false);
+            // Delete input fields after the message send
+            sendEmail(values).then(() => resetForm({ values: "" }));
+          }}
+        >
+          <Form className={styles.form__container}>
+            <div className={styles.form__container__name}>
+              <MyTextInput
+                label="Full Name"
+                id="name"
+                name="name"
+                type="text"
+                placeholder="Full Name"
+                className={styles.form__input__name}
+              />
+            </div>
+            <div className={styles.form__container__email}>
+              <MyTextInput
+                label="Email Address"
+                id="email"
+                name="email"
+                type="email"
+                placeholder="Email Address"
+              />
+            </div>
+            <div className={styles.form__container__message}>
+              <MyTextArea
+                label="Message"
+                name="message"
+                id="message"
+                placeholder="Message"
+                rows="4"
+                cols="50"
+              />
+            </div>
 
-          <button type="submit" className={styles.form__container__btnSubmit}>
-            Submit
-          </button>
-        </Form>
-      </Formik>
+            <button type="submit" className={styles.form__container__btnSubmit}>
+              Submit
+            </button>
+          </Form>
+        </Formik>
+      )}
     </section>
   );
 };
