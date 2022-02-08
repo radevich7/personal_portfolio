c5a7b6eaecd18c484b18e23ab26e39d135949844
Julian Radevych <79290689+radevich7@users.noreply.github.com>
2022-02-08 15:02:47 -0700

    <Fix:change text content for about page>

--- components/skills/Skills.js
+++ components/skills/Skills.js
@@ -9,14 +9,12 @@ const Skills = () => {
           I pivot into the web development industry in the end of 2020. Since
           that time, I was exposed to the multiple projects and technologies,
           that challenged me and help to gain experience and a strong
-          foundation. Recently, I completed Launchpad program by VOG (Calgary,
-          AB), where I worked with the other team members to build an online
-          marketplace for the resale of goods. This program helped me to
-          advanced my skills and understand of software development in Agile
-          work environment.
-          {/* I was so
-          hungry to learn coding that I decided to quit from my previous job and
-          invest all of my time and effort in order to become web developer. */}
+          foundation. <br />
+          <br />
+          Recently, I completed Launchpad program by VOG (Calgary, AB), where I
+          worked with the other team members to build an online marketplace for
+          the resale of goods. This program helped me to advanced my skills and
+          learn how to work in Agile work environment.
         </p>
       </div>
       <div className={styles.skills__stack}>
